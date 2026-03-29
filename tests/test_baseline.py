d652e95d629fba6f2b97d0414c4846b9c76ee8dc
David Nobles <davidnobles.eng@gmail.com>
2026-03-29 00:00:00 -0700

    fix test

--- tests/test_baseline.py
+++ tests/test_baseline.py
@@ -1,10 +1,12 @@
+import io
+import math
+
 import allure
-import pytest
+import matplotlib
 import numpy as np
+import pytest
 
-import matplotlib.pyplot as plt
-
-plt.use("Agg")
+matplotlib.use("Agg")
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
@@ -13,17 +15,12 @@ from matplotlib.figure import Figure
 def test_true():
     assert True
 
-@pytest.mark.xfail
-def test_xfail():
-    assert False
-
-
 def test_attach_sine_wave_to_allure_report():
-    x_values = [np.tau * x / 199 for x in range(200)]
+    x_values = [math.tau * x / 199 for x in range(200)]
     y_values = [np.sin(x) for x in x_values]
 
-    plt.figure = Figure(figsize=(6, 3), dpi=100)
-    plt.FigureCanvasAgg(figure)
+    figure = Figure(figsize=(6, 3), dpi=100)
+    FigureCanvasAgg(figure)
     axis = figure.subplots()
     axis.plot(x_values, y_values, color="#1f77b4", linewidth=2)
     axis.axhline(0, color="#d9d9d9", linewidth=1)
